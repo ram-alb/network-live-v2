d2c4303a7580ba1c5d1243f67de50b850820df6e
Ramil Albakov <ramil.albakov2@gmail.com>
2023-12-09 16:00:10 +0600

    test: modify tests for Beeline Huawei LTE becuase of additional parameters

--- tests/beeline/huawei/test_parse_lte.py
+++ tests/beeline/huawei/test_parse_lte.py
@@ -18,10 +18,16 @@ def sort_list_of_dicts(data):
     return sorted(data, key=lambda item: item['cell_name'])
 
 
+def delete_date(data):
+    for item in data:
+        del item['insert_date']
+
+
 @pytest.mark.parametrize('sharing_type', ['moran', 'mocn'])
 def test_parse_lte_cms(sharing_type):
     with open(json_results[sharing_type], 'r') as json_result:
         expected = json.load(json_result)
     lte_cells = parse_lte_cms(xmls_path, sharing_type, physical_params)
+    delete_date(lte_cells)
 
     assert sort_list_of_dicts(lte_cells) == sort_list_of_dicts(expected)
